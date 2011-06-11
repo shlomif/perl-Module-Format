d8d1e74c8f860143845cf69e64dd7f8ccbb851d5
Shlomi Fish <shlomif@shlomifish.org>
2011-06-11 16:41:45 +0300

    Bump the version number to 0.0.4.

--- Module-Format/Module-Format/lib/Module/Format.pm
+++ Module-Format/Module-Format/lib/Module/Format.pm
@@ -10,11 +10,11 @@ Perl module names and distribution names are kept in.
 
 =head1 VERSION
 
-Version 0.0.3
+Version 0.0.4
 
 =cut
 
-our $VERSION = '0.0.3';
+our $VERSION = '0.0.4';
 
 =head1 SYNOPSIS
 
--- Module-Format/Module-Format/lib/Module/Format/Module.pm
+++ Module-Format/Module-Format/lib/Module/Format/Module.pm
@@ -9,11 +9,11 @@ Module::Format::Module - encapsulates a single Perl module.
 
 =head1 VERSION
 
-Version 0.0.3
+Version 0.0.4
 
 =cut
 
-our $VERSION = '0.0.3';
+our $VERSION = '0.0.4';
 
 =head1 SYNOPSIS
 
