85461b86c999cadabe07faa10e2b05d5d6e1e4b0
Shlomi Fish <shlomif@shlomifish.org>
2013-05-21 19:48:16 +0300

    Bump version number to 0.0.5

--- Module-Format/Module-Format/lib/Module/Format.pm
+++ Module-Format/Module-Format/lib/Module/Format.pm
@@ -10,11 +10,11 @@ Perl module names and distribution names are kept in.
 
 =head1 VERSION
 
-Version 0.0.4
+Version 0.0.5
 
 =cut
 
-our $VERSION = '0.0.4';
+our $VERSION = '0.0.5';
 
 =head1 SYNOPSIS
 
--- Module-Format/Module-Format/lib/Module/Format/ModuleList.pm
+++ Module-Format/Module-Format/lib/Module/Format/ModuleList.pm
@@ -9,11 +9,11 @@ Module::Format::ModuleList - an ordered list of L<Module::Format::Module>.
 
 =head1 VERSION
 
-Version 0.0.4
+Version 0.0.5
 
 =cut
 
-our $VERSION = '0.0.4';
+our $VERSION = '0.0.5';
 
 =head1 SYNOPSIS
 
