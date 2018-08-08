2c38a784b189382e7af89832fa023eef1858b380
Shlomi Fish <shlomif@shlomifish.org>
2018-08-08 13:41:57 +0300

    update script/tag-release.pl for git/dzil.

--- Module-Format/Module-Format/script/tag-release.pl
+++ Module-Format/Module-Format/script/tag-release.pl
@@ -6,22 +6,18 @@ use warnings;
 use IO::All;
 
 my ($version) =
-    (map { m{\$VERSION *= *'([^']+)'} ? ($1) : () }
-    io->file('lib/Module/Format.pm')->getlines()
-    )
-    ;
+    ( map { m{\Aversion *= *(\S+)\n?\z} ? ($1) : () }
+        io->file("./dist.ini")->getlines() );
 
-if (!defined ($version))
+if ( !defined($version) )
 {
     die "Version is undefined!";
 }
 
 my @cmd = (
-    "hg", "tag", "-m",
-    "Tagging the Module-Format release as $version",
+    "git", "tag", "-m", "Tagging the Module-Format release as $version",
     "Module-Format-v$version",
 );
 
-print join(" ", map { /\s/ ? qq{"$_"} : $_ } @cmd), "\n";
+print join( " ", map { /\s/ ? qq{"$_"} : $_ } @cmd ), "\n";
 exec(@cmd);
-
